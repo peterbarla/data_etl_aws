d75f0c1d53e538d3465bc837983f8d6bc2b6876c
peter.barla <ppepecsek@yahoo.com>
2020-08-01 13:09:23 +0300

    moved credentials locally

--- serverless-lambda/redshifter.py
+++ serverless-lambda/redshifter.py
@@ -4,21 +4,23 @@ import sys
 import boto3
 import datetime
 
+# OPEN FILE WITH CREDENTIALS
+lines = []
+with open('credentials.txt', 'rt') as f:  
+    lines = f.readlines()
+
 RS_PORT = 5439
-RS_USER = 'peter.barla'
-DATABASE = 'peterdb'
-CLUSTER_ID = 'redshift-peter-1'
-RS_HOST = 'redshift-peter-1.cncdopxmun4t.us-east-2.redshift.amazonaws.com'
+RS_USER = lines[0].strip()
+DATABASE = lines[1].strip()
+CLUSTER_ID = lines[2].strip()
+RS_HOST = lines[3].strip()
+
 client = boto3.client('redshift')
 cluster_creds = client.get_cluster_credentials(DbUser=RS_USER,
                                            DbName=DATABASE,
                                       ClusterIdentifier=CLUSTER_ID,
                                            AutoCreate=False)
 def build_and_send_query(category: str, filename: str, lwm: str, hwm: str, timestamp: str)-> str:
-
-    # CREATING INSERT QUERY
-    #REDSHIFT_QUERY = f"INSERT INTO peter_schema.staging_uploaded_files(category, filename, lwm, hwm, timestamp) VALUES({category}, {filename}, {lwm}, {hwm}, {timestamp})"
-
 
     # CREATING A CONENCTION TO THE REDSHIFT CLUSTER
     try:
@@ -32,14 +34,12 @@ def build_and_send_query(category: str, filename: str, lwm: str, hwm: str, times
         print(ERROR)
         sys.exit(1)
     print('AFTER CONENCTION')
-    #print(type(REDSHIFT_QUERY))
     # EXECUTING THE QUERY
     try:
         cursor = conn.cursor()
         print(cursor.execute('INSERT INTO peter_schema.staging_uploaded_files (category, filename, lwm, hwm, timestamp) VALUES (%s, %s, %s, %s, %s)', 
             (category, filename, datetime.datetime.strptime(lwm, '%d%m%Y'), 
             datetime.datetime.strptime(hwm, '%d%m%Y'), datetime.datetime.strptime(timestamp, '%d/%m/%Y %H:%M:%S') )))
-        #print(cursor.execute(REDSHIFT_QUERY))
         cursor.close()
         conn.commit()
         conn.close()
